f45f71fd34e82113ea3e39666624278cdc7953bd
zayatsdan <36342659+zayatsdan@users.noreply.github.com>
2023-11-22 16:20:43 +0100

    Update GreatestCommonDivisor.java

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,5 +2,26 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
+         if(second>first&&first!=0&&second!=0) {
+                for (int devaider = second; devaider >= 1; devaider--) {
+                    if (second % (devaider - 1) == 0) {
+                        if (first % (devaider - 1) == 0) {
+                            System.out.println(devaider - 1);
+                            break;
+                        }
+                    }
+                }
+            } else if (first>second&&first!=0&&second!=0) {
+                for (int devaider = first; devaider >= 1; devaider--) {
+                    if (first % (devaider - 1) == 0) {
+                        if (second % (devaider - 1) == 0) {
+                            System.out.println(devaider - 1);
+                            break;
+                        }
+                    }
+                }
+            }else if(first==second) {
+                System.out.println(first);
+            }
     }
 }
