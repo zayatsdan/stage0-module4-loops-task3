bf673f398dbad5237d2ab63d9b3e4a59f31bdfb9
zayatsdan <36342659+zayatsdan@users.noreply.github.com>
2023-11-22 16:20:02 +0100

    Update AlphabetPrinter.java

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+        int counter='a';
+        while (counter<='z'){
+            char x= (char) counter;
+            System.out.println(x);
+            counter++;
+        }
     }
 }
